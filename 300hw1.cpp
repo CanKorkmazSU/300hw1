4fc67fd6211d58242cd2a3950d817cbd14d7b58c
StylishGentleman <74717269+StylishGentleman@users.noreply.github.com>
2021-07-15 16:52:05 +0300

    Add project files.

---
+++ 300hw1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+
+int main()
+{
+	int K, M, N;
+	cout << "enter K,M,N" << endl;
+	cin >> K >> M >> N;
+
+
+
+
+}
+
---
+++ Maze.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Maze
+{
+public:
+	Maze(int X, int Y) : M(X), N(Y) {}
+
+
+	
+
+private:
+	int K, M, N, entryX, entryY;
+	
+};
+
